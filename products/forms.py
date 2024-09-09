a4d7d0c4e5350688055ba0a7973bc7bea9d02e69
Tomas Coa <tomasjaviercm@gmail.com>
2024-09-09 01:29:51 -0300

    Creacion de formulario, vista y URLS

---
+++ products/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import Product
+
+# Estos formularios me permiten ejecutar acciones cuando el usuario haga SUBMIT, podemos
+# crear un metodo que se llame save() para guardar esto en el modelo
+
+class ProductForm(forms.Form):
+    name = forms.CharField(max_length=200, label='Nombre')
+    description = forms.CharField(max_length=300, label='Descripcion')
+    price = forms.DecimalField(max_digits=10, decimal_places=2, label='Precio')
+    available = forms.BooleanField(initial=True, label='Disponibilidad', required=False)
+    photo = forms.ImageField(label='Foto', required=False)
+
+
+    # cleaned_data[] es un diccionario que nos permite traer la informacion limpia del formulario
+    # cuando el usuario hace el request.
+       
+    def save(self):
+        Product.objects.create(
+            name = self.cleaned_data['name'],
+            description = self.cleaned_data['description'],
+            price = self.cleaned_data['price'],
+            available = self.cleaned_data['available'],
+            photo = self.cleaned_data['photo'],
+        )
--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views import generic
+from .forms import ProductForm
+from django.urls import reverse_lazy
 
-# Create your views here.
+class ProductFormView(generic.FormView):
+    template_name = 'products/add_product.html'
+    form_class = ProductForm
+    success_url = reverse_lazy('add_product') # Este atributo funciona como una redireccion cuando el submit sea exitoso
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form) # Este método generalmente redirige al usuario a la success_url
+    
