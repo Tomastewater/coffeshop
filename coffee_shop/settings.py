2cdab82b0acea86656c53577afe49d70a7417a80
Tomas Coa <tomasjaviercm@gmail.com>
2024-12-06 00:32:44 -0300

    Se agrego el modulo de registro de usuario, tambien se hizo modificaciones en el header en base.html

--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -113,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ar'
 
 TIME_ZONE = 'UTC'
 
@@ -137,3 +137,5 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
 CRISPY_TEMPLATE_PACK = "tailwind"
 
 LOGIN_REDIRECT_URL = 'list_product'
+
+LOGOUT_REDIRECT_URL = 'login'
